1ba66a1b97d80fc238b909ad164461d49eda9c6d
Yehiel Hamou <balolo4490@gmail.com>
2022-04-01 18:04:17 +0300

    updating our app with a shop page, collection preview component and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -9,36 +9,36 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: 'Hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats',
-        },
-        {
-          title: 'Jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets',
-        },
-        {
-          title: 'Sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers',
-        },
-        {
           title: 'WOMENS',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
-          id: 4,
+          id: 1,
           linkUrl: 'shop/womens',
         },
         {
           title: 'MENS',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
-          id: 5,
+          id: 2,
           linkUrl: 'shop/mens',
+        },
+        {
+          title: 'Hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 3,
+          linkUrl: 'shop/hats',
+        },
+        {
+          title: 'Jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 4,
+          linkUrl: 'shop/jackets',
+        },
+        {
+          title: 'Sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          id: 5,
+          linkUrl: 'shop/sneakers',
         },
       ],
     };
